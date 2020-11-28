27d5b7f0059812830615d4a3e2c1a66fe94db384
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2020-11-29 01:56:34 +0500

    gym fornt page design face one

--- JavaScript With Harray/Express/app.js
+++ JavaScript With Harray/Express/app.js
@@ -1,28 +1,48 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = 80;
 
-// Express spacific stuff
-app.use("/static", express.static("static")); // For serving staitic files
 
-// Express spacific pug
-app.set('view engine', 'pug'); // set the template engin as pug
-app.set('view ', path.join(__dirname, 'view ')); // set the views directory
+//EXPRESS SPACIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
+
+
+// PUG SPACIFIC STUFF
+app.set('view engine', 'pug'); // set the template engine as pug 
+app.set('views', path.join(__dirname, 'views')); // set the views directory
+
 
 // ENDPOINTS
 app.get("/", (req, res) => {
-    const con = "This is the best content on the internet so far to use it wisely";
-    const params = {
-        "title": "Pug is the best",
-        "content"
+    const con = "This is the best content of the internet so far  so use it wisely "
+    const param = {
+        "title": "Pug is the Best",
+        "content": con
+    };
+    res.status(200).render('index.pug', param);
+})
 
 
-        : con
-    }
-    res.status(200).render("index.pug", params);
+
+
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`This application started successfully ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,31 +56,19 @@ app.get("/demo", (req, res) => {
         message: 'Hello there!'
     })
 });
-
 app.get("/", (req, res) => {
     res.send("This is homepage and my first express app ");
 });
-
 app.get("/about", (req, res) => {
     res.send("This is about page and my first express app ");
 });
-
 app.get("/contact", (req, res) => {
     res.send("This is contact page and  my first express app ");
 });
-
 app.get("/this", (req, res) => {
     res.status(404).send("This page is not found 404");
 });
-
 app.post("/contact", (req, res) => {
     res.send("This is contact page and  my first express app ");
 });
-
 */
-
-
-// Start the server
-app.listen(port, () => {
-    console.log(`This application started successfuly on port  ${port}`);
-});
