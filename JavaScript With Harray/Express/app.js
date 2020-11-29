deb3db2a887944e0a3eb0afc9fcee09fd0db4662
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2020-11-29 14:29:35 +0500

    add sumbit data in a file

--- JavaScript With Harray/Express/app.js
+++ JavaScript With Harray/Express/app.js
@@ -1,6 +1,10 @@
+const {
+    urlencoded
+} = require("express");
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const port = 80;
 
 
@@ -11,7 +15,7 @@ app.use('/static', express.static('static')); // For serving static files
 // PUG SPACIFIC STUFF
 app.set('view engine', 'pug'); // set the template engine as pug 
 app.set('views', path.join(__dirname, 'views')); // set the views directory
-
+app.use(urlencoded());
 
 // ENDPOINTS
 app.get("/", (req, res) => {
@@ -23,7 +27,21 @@ app.get("/", (req, res) => {
     res.status(200).render('index.pug', param);
 })
 
+app.post("/", (req, res) => {
+    let name = req.body.name;
+    let age = req.body.age;
+    let address = req.body.address;
+    let gender = req.body.gender;
+    let more = req.body.more;
+    console.log(req.body);
 
+    let outputToWrite = `The name of the client is ${name} , ${age} year old , ${gender} ,residing at ${address} . More about him/her : ${more}`;
+    fs.writeFileSync("output.txt", outputToWrite);
+    const params = {
+        "message ": "Your form has been sumbitted successfully"
+    };
+    res.status(200).render('index.pug', params);
+})
 
 
 
