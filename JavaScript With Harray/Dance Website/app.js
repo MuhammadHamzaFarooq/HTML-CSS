e075398ea219a8b8414370bf7fa680343b20d1be
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2020-11-30 23:17:49 +0500

    dance websiete

---
+++ JavaScript With Harray/Dance Website/app.js
@@ -0,0 +1,27 @@
+const {
+    urlencoded
+} = require("express");
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8000;
+
+//EXPRESS SPACIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
+
+
+// PUG SPACIFIC STUFF
+app.set('view engine', 'pug'); // set the template engine as pug 
+app.set('views', path.join(__dirname, 'views')); // set the views directory
+app.use(urlencoded());
+
+// ENDPOINTS
+app.get("/", (req, res) => {
+    const param = {};
+    res.status(200).render('index.pug', param);
+});
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`This application started successfully ${port}`);
+});
