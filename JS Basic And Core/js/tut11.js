579b0bf02b0dc07541eb3d74e3135b54d253ee7d
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2020-12-07 13:40:23 +0500

    add js

---
+++ JS Basic And Core/js/tut11.js
@@ -0,0 +1,16 @@
+// This is tutorial 11 and study for window object
+let a = window; // windwo ka global scope hota hay
+console.log(a);
+
+
+let con = confirm("Are you sure you want to delete this page");
+console.log(con);
+
+
+a = window.innerHeight;
+a = window.innerWidth;
+a = scrollX;
+a = location.toString;
+a = window.history
+
+console.log(a);
---
+++ JS Basic And Core/js/tut6.js
@@ -0,0 +1,15 @@
+// string methods and template literal tutorials
+let string = "pakistan zindabad";
+console.log(string.indexOf("z"));
+console.log(string.lastIndexOf("a"));
+console.log(string.endsWith("zindabad"));
+console.log(string.charAt("5"));
+console.log(string[5]);
+console.log(string.includes("zindabad"));
+console.log(string.substring(0,10));
+console.log(string.slice(0,4));
+console.log(string.slice(0,4));
+console.log(string.split('a'));
+// This function replace first accurence
+console.log(string.replace('pakistan','pakistani')); 
+
